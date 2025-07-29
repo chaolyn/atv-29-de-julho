0879f86c1d4eb04b3e873a994f3d9b38d72b0820
chaolyn <wendel.alexandre.silva@escola.pr.gov.br>
2025-07-29 11:59:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*function nome(){
+    let nome = prompt("digite seu nome");
+    alert("nome");
+}*/
+
+function nomeNoDocumento(){
+    let nome = prompt("digite seu nome");
+    document.write(nome);
+
+}
